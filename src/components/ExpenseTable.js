d84c56abb726036c9635a98472197e634e8c2ec2
Cesar Preza <cpreza93@yahoo.com>
2020-12-07 23:05:46 -0800

    styling added

--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -2,9 +2,19 @@ import React from 'react'
 
 
 function ExpenseTable() {
+
     return (
         <div>
-            
+            <table>
+                <thead className='tableHeader'>
+                <tr>
+                    <th>What did you buy?</th>
+                    <th>Where did you buy it?</th>
+                    <th>How much did you spend?</th>
+                    <th>When was your purchase?</th>
+                </tr>
+                </thead>
+            </table>
         </div>
     )
 }
