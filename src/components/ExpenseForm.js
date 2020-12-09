22e78c8209675785745b9bc5decf7cc1b1638966
Cesar Preza <cpreza93@yahoo.com>
2020-12-08 20:32:09 -0800

    insert table data

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -4,16 +4,15 @@ function ExpenseForm() {
     return (
         <div>
             <form className = 'expense-form'>
-                <input type= 'text' placeholder='What did you buy?' />
-                
-                <select>
-                    <option value='credit card'>Credit Card</option>
+                <select style={{borderRadius: 5}} >
                     <option value='cash'>Cash</option>
+                    <option value='credit card'>Credit Card</option>
                     <option value='crypto'>Crypto</option>
                 </select>
-                <input type='date'></input>
-                <input type='text' placeholder= 'Where did you buy it?'></input>
-                <input type='submit'></input>
+                <input style={{width: 200, borderRadius: 5,}} type= 'text' placeholder='What did you buy?' />
+                <input style={{width: 200, borderRadius: 5}} type='text' placeholder= 'Where did you buy it?'></input>
+                <input style={{borderRadius: 5}} type='date'></input>
+                <input style={{width: 100, borderRadius: 5}} type='submit'></input>
             </form>
         </div>
     )
--- src/components/ExpenseTable.js
+++ src/components/ExpenseTable.js
@@ -5,15 +5,19 @@ function ExpenseTable() {
 
     return (
         <div className='table'>
-            <table className='tableHeader'>
+            <table className='table-data'>
                 <thead>
                 <tr>
                     <th>What did you buy?</th>
                     <th>Where did you buy it?</th>
                     <th>How much did you spend?</th>
                     <th>When was your purchase?</th>
+                    <th>Delete Expense</th>
                 </tr>
                 </thead>
+                <tbody>
+                    <tr>dummy data</tr>
+                </tbody>
             </table>
         </div>
     )
